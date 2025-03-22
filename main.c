0001841e76138e3b47562eacfc3f7784947e57f4
ThatOneGuy030 <johnathanltablet@gmail.com>
2025-03-21 23:13:08 -0500

    I keep running into errors regarding hid.

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "bsp/board.h"
+#include "tusb.h"
+
+// GPIO matrix
+const uint8_t gpio_pins[3][3] = {
+    {28, 2, 5}, // Top row
+    {27, 11, 7}, // Middle row
+    {19, 17, 14} // Bottom row
+};
+
+// Keyboard matrix
+const uint8_t keycodes[3][3] = {
+    {HID_USAGE_KEY_1, HID_KEY_2, HID_KEY_3},
+    {HID_KEY_4, HID_KEY_5, HID_KEY_6},
+    {HID_KEY_7, HID_KEY_8, HID_KEY_9}
+};
 
 
 int main()
