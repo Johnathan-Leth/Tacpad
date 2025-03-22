b93d69df7bf5f0b3256c2f9478aa9206991f865e
ThatOneGuy030 <johnathanltablet@gmail.com>
2025-03-22 15:33:19 -0500

    Code won't compile. Will look into

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 //#include "bsp/board.h"
 #include "tusb.h"
 
+#define LED_PIN 25
+
 // GPIO matrix
 const uint8_t gpio_pins[3][3] = {
     {28, 2, 5}, // Top row
@@ -17,13 +19,52 @@ const uint8_t keycodes[3][3] = {
     {HID_KEY_7, HID_KEY_8, HID_KEY_9}
 };
 
+// Button monitor
+void gpio_caller(uint gpio, uint32_t events) {
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            if (gpio_pins[row][col] == gpio) {
+                uint8_t key = keycodes[row][col];
 
-int main()
-{
+                // Sends button press and flashes led for testing
+                if (tud_hid_ready()) {
+                    uint8_t report[6] = {key, 0, 0, 0, 0, 0};
+                    tud_hid_keyboard_report(0, 0, report);
+                    gpio_put(LED_PIN, 1);
+
+                    sleep_ms(100);
+
+                    uint8_t empty_report[6] = {0};
+                    tud_hid_keyboard_report(0, 0, empty_report);
+                    gpio_put(LED_PIN, 1);
+                }
+            }
+        }
+    }
+}
+
+// Initialize gpio pins to monitor
+void init_gpio() {
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            uint8_t pin = gpio_pins[row][col];
+            gpio_init(pin);
+            gpio_set_dir(pin, GPIO_IN);
+            gpio_pull_up(pin);
+            gpio_set_irq_enabled_with_callback(pin, GPIO_IRQ_EDGE_FALL, true, &gpio_caller);
+        }
+    }
+}
+
+// Initialize and loop the 
+int main() {
     stdio_init_all();
+    tusb_init();
+    init_gpio();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        tud_task();
     }
 }
